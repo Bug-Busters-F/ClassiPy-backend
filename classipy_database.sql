bf14778e6d3d7369a723b67ed9a2aa2f34379111
Davi Miyake Batista <165038725+DaviMBDev@users.noreply.github.com>
2025-09-18 10:35:44 -0300

    feat:sql code add

---
+++ classipy_database.sql
@@ -0,0 +1,24 @@
+create database classipy;
+\c database classipy;
+
+create table tipi(
+id_tipi serial primary key,
+descricao varchar(100),
+aliquota numeric(5,2),
+ncm char(8)
+);
+
+create table produto(
+part_number varchar(25) primary key,
+nome varchar(100),
+fornecedor varchar(100),
+pais_origem varchar(2),
+id_tipi int,
+constraint fk_id_tipi foreign key (id_tipi) references tipi(id_tipi)
+);
+
+create table historico(
+process_data date,
+part_number varchar(25),
+constraint fk_part_number foreign key (part_number) references produto(part_number)
+);
