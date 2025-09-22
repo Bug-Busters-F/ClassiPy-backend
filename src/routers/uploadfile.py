8d7818d3c8236fed273504af7dcb2e8a5ee46d9c
Gabriel Viell Castilho <gabrielviell@hotmail.com>
2025-09-22 09:37:12 -0300

    update: return hash code

--- src/routers/uploadfile.py
+++ src/routers/uploadfile.py
@@ -26,10 +26,8 @@ async def create_upload_file(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_DIR, f"{file_hash}{ext}")
 
     if os.path.exists(file_path):
-        raise HTTPException(status_code=400, detail="Arquivo já enviado")
-
-    with open(file_path, "wb") as f:
-        f.write(content)
+        with open(file_path, "wb") as f:
+            f.write(content)
 
     try:
         if ext.lower() == ".pdf":
@@ -44,8 +42,11 @@ async def create_upload_file(file: UploadFile = File(...)):
                     result = read_pdf.find_pn(page)
         else:
             result = "Arquivo não é PDF. Apenas PDFs são analisados."
+            return json.load(result)
 
-        return json.loads(result)
+        response = json.loads(result)
+        response["hash_code"] = file_hash
+        return response
     except Exception as e:
         if os.path.exists(file_path):
             os.remove(file_path)
